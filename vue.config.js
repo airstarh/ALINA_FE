5648dfbd36ee26729ae676a0d86b82c66c1256d8
Vsevolod Azovsky <vsevolod.azovskiy@dataart.com>
2019-06-22 16:59:48 +0300

    Built and Run from file successfully!!!

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	// Deprecated since Vue CLI 3.3, please use publicPath instead
+	// publicPath
+	baseUrl: '.',
+	publicPath: '.',
+	devServer: {
+		clientLogLevel: 'info'
+	  }
+};
