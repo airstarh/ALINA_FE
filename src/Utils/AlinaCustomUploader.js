79c3824b6e288dc01b624fc6dd9c70ddfa4c9c01
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2020-04-29 09:06:22 +0300

    dev

--- src/Utils/AlinaCustomUploader.js
+++ src/Utils/AlinaCustomUploader.js
@@ -57,15 +57,19 @@ export class MyUploadAdapter {
             // Your integration may handle upload errors in a different way so make sure
             // it is done properly. The reject() function must be called when the upload fails.
             if (!response || response.error || (response.meta && response.meta.alina_response_success == 0)) {
-                // if (response.messages) {
-                //     let msgs = [];
-                //     msgs = msgs.concat(response.messages);
-                //     msgs.forEach((item) => {
-                //         MessagesObj.add(item);
-                //     });
-                // }
                 return reject(response && response.error ? response.error.message : genericErrorText);
             }
+
+            //region ALINA
+            // if (response.messages) {
+            //     let msgs = [];
+            //     msgs = msgs.concat(response.messages);
+            //     msgs.forEach((item) => {
+            //         MessagesObj.add(item);
+            //     });
+            // }
+            //endregion ALINA
+
             // If the upload is successful, resolve the upload promise with an object containing
             // at least the "default" URL, pointing to the image on the server.
             // This URL will be used to display the image in the content. Learn more in the
