07d23e8d350ec4a265c171a5f91a622465c659c0
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2020-05-29 07:33:24 +0300

    Revert to CLeditor 18; build passes

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,8 @@ const {styles}              = require('@ckeditor/ckeditor5-dev-utils');
 module.exports              = {
     // Deprecated since Vue CLI 3.3, please use publicPath instead
     //baseUrl: '.',
-    publicPath:            '.',
-    //publicPath:            '/apps/vue/',
+    //publicPath:            '.',
+    publicPath:            '/apps/vue/',
     devServer:             {
         public:           'https://localhost:8082',
         https:            true,
@@ -27,18 +27,18 @@ module.exports              = {
                 // Additional languages that will be emitted to the `outputDirectory`.
                 // This option can be set to an array of language codes or `'all'` to build all found languages.
                 // The bundle is optimized for one language when this option is omitted.
-                additionalLanguages:                    ['ru'],
+                //additionalLanguages:                    ['ru'],
                 // Optional directory for emitted translations. Relative to the webpack's output.
                 // Defaults to `'translations'`.
-                outputDirectory:                        'ckeditor5-translations',
+                //outputDirectory:                        'translations',
                 // Whether the build process should fail if an error occurs.
                 // Defaults to `false`.
                 //strict:                                 true,
                 // Whether to log all warnings to the console.
                 // Defaults to `false`.
-                verbose:                                true,
-                addMainLanguageTranslationsToAllAssets: true,
-                buildAllTranslationsToSeparateFiles:    true,
+                //verbose:                                true,
+                // addMainLanguageTranslationsToAllAssets: true,
+                // buildAllTranslationsToSeparateFiles:    true,
             })
         ]
     },
