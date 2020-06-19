4003253ffff5bdb712856a71f2e4fcc1f5343d22
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2020-06-19 15:32:59 +0300

    alina date picker fix 2

--- src/components/elements/form/AlinaDatePicker.vue
+++ src/components/elements/form/AlinaDatePicker.vue
@@ -1,30 +1,37 @@
 <template>
 
-    <div class="input-group mb-2 ">
-        <div class="row no-gutters align-items-center">
-            <div class="col-auto mt-2" v-if="label.length">
+    <div class="alina-date-picker mb-2 ">
+        <div class="row no-gutters align-items-center justify-content-between">
+            <div class="col-auto" v-if="label.length">
                 <div class="input-group-text">{{label}}</div>
             </div>
             <div class="col">
-                <div class="row no-gutters">
-                    <div class="col-lg text-nowrap mt-2 pl-3 pr-3">
-                        <div class="form-row">
-                            <div class="col-6 p-0"><input v-model="year" size="4" type="text" max="9999" class="form-control text-right" :id="`year-${idq}`" placeholder="YEAR"></div>
-                            <div class="col-3 p-0"><input v-model="month" size="2" type="text" max="12" class="form-control text-right" :id="`month-${idq}`" placeholder="MONTH"></div>
-                            <div class="col-3 p-0"><input v-model="day" size="2" type="text" max="31" class="form-control text-right" :id="`day-${idq}`" placeholder="DAT"></div>
+                <div class="container">
+                    <div class="row no-gutters align-items-center justify-content-start">
+                        <div class="col-auto text-nowrap p-3">
+                            <div class="form-inline">
+                                <div class="text-nowrap">
+                                    Y:<input v-model="year" size="4" type="text" max="9999" class="form-control d-inline-block w-25" :id="`year-${idq}`" placeholder="YEAR">
+                                    M:<input v-model="month" size="2" type="text" max="12" class="form-control d-inline-block w-25" :id="`month-${idq}`" placeholder="MONTH">
+                                    D:<input v-model="day" size="2" type="text" max="31" class="form-control d-inline-block w-25" :id="`day-${idq}`" placeholder="DAT"></div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-lg text-nowrap mt-2 pl-3 pr-3">
-                        <div class="form-row ">
-                            <div class="col-4 p-0"><input v-model="hour" size="2" type="text" max="23" class="ch2 form-control" :id="`hour-${idq}`" placeholder="HOUR"></div>
-                            <div class="col-4 p-0"><input v-model="min" size="2" type="text" max="59" class="ch2 form-control" :id="`min-${idq}`" placeholder="MIN"></div>
-                            <div class="col-4 p-0"><input v-model="sec" size="2" type="text" max="59" class="ch2 form-control" :id="`sec-${idq}`" placeholder="SEC"></div>
+                        <div class="col-auto text-nowrap p-3">
+                            <div class="form-inline">
+                                <div class="text-nowrap">
+                                    h:<input v-model="hour" size="2" type="text" max="23" class="ch2 form-control d-inline w-25" :id="`hour-${idq}`" placeholder="HOUR">
+                                    m:<input v-model="min" size="2" type="text" max="59" class="ch2 form-control d-inline w-25" :id="`min-${idq}`" placeholder="MIN">
+                                    s:<input v-model="sec" size="2" type="text" max="59" class="ch2 form-control d-inline w-25" :id="`sec-${idq}`" placeholder="SEC">
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-lg text-nowrap mt-2">
-                        <div class="input-group-text">
-                            <i>{{emittedRes | unix_to_date_time}}</i>
+                        <div class="col-auto text-nowrap p-3">
+                            <div class="input-group-text">
+                                <i>{{emittedRes | unix_to_date_time}}</i>
+                            </div>
+                            <span @click="setNow" class="btn btn-sm btn-primary">SET NOW</span>
                         </div>
+
                     </div>
                 </div>
             </div>
@@ -79,14 +86,22 @@
                 this.emittedRes = Math.floor(this.dateObj.getTime() / 1000);
                 this.$emit('input', this.emittedRes);
             },
-            convertValueToDate() {
-                this.dateObj = new Date(this.value * 1000);
+            convertValueToDate(v = null) {
+                if (v === null) {
+                    v = this.value;
+                }
+                this.dateObj = new Date(v * 1000);
                 this.year    = this.dateObj.getFullYear();
                 this.month   = this.dateObj.getMonth() + 1;
                 this.day     = this.dateObj.getDate();
                 this.hour    = this.dateObj.getHours();
                 this.min     = this.dateObj.getMinutes();
                 this.sec     = this.dateObj.getSeconds();
+            },
+            setNow(){
+                this.emittedRes = Math.floor((new Date()).getTime() / 1000);
+                this.convertValueToDate(this.emittedRes);
+                this.$emit('input', this.emittedRes);
             }
         },
         watch:   {
