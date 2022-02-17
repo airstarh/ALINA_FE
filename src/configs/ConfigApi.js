bcbfd535ef981a1c95f1f9185248c7a676b71172
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2022-02-18 02:35:43 +0300

    LOCALHOST

--- src/configs/ConfigApi.js
+++ src/configs/ConfigApi.js
@@ -1,8 +1,8 @@
 import UtilsSys from "@/Utils/UtilsSys";
 const ConfigApi = {
-    "url_base": "https://saysimsim.ru",
+    //"url_base": "https://saysimsim.ru",
     //"url_base":              "https://borg-001.private",
-    //"url_base": "https://alinazero",
+    "url_base": "https://alinazero",
     //"url_base":              "https://host.home",
     //"url_base":              "https://192.168.1.110",
     //"url_base":              "https://vov",
