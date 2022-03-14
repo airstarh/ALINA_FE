d796779a342ccb8954dd4790a491b0cedfffc3ec
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2022-03-14 06:29:36 +0300

    m45a

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,25 +1,12 @@
 <template>
   <div class="row no-gutters mt-5 alina-form p-5">
     <div class="col">
-      <br>
-      Здесь люди просто тусят
-      <br>
-      Коллектив индивидуалистов
-      <br>
-      Славные ублюдки
-      <br>
-      Идейная синь
-      <br>
-      ОчУмелые ручки
-      <br>
-      Лесная аристократия
-      <br>
-    </div>
-    <div class="col">
-      <br>
-      Ведущий проекта: <a href="https://vk.com/vsevolod.azovsky">Всеволод Азовский</a>
-      <br>
-      Муза куража: <a href="https://www.facebook.com/sllimrut">Slimrut</a>
+      <h3>
+        Официальнй сайт TCH "ТСЖ Миронова 45 А"
+      </h3>
+      <p>
+        394005, Воронежская область, город Воронеж, улица Миронова 45 А
+      </p>
       <br>
     </div>
   </div>
--- src/components/MenuHorizontalMain.vue
+++ src/components/MenuHorizontalMain.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="mb-1" id="alina-main-menu">
-    <b-navbar toggleable="lg" type="dark" class="corporate-bg-and-text">
-      <b-navbar-brand to="/">¯\_(ツ)_/¯</b-navbar-brand>
+    <b-navbar toggleable="lg" type="light" class="corporate-bg-and-text">
+      <b-navbar-brand to="/">
+        Миронова 45 А
+        <sup>официальный сайт</sup>
+      </b-navbar-brand>
       <b-navbar-nav class="flex-row">
         <b-nav-item to="/tale/upsert" v-if="CU.isLoggedIn()" class="mr-3">
           <b-icon-plus-circle-fill></b-icon-plus-circle-fill>
@@ -14,63 +17,20 @@
               style="font-size: .8rem; line-height: 1.2rem; height:1.2rem; vertical-align: middle; left:-.3rem"
           >&nbsp;{{ CU.attributes.count_notifications }}&nbsp;</sup>
         </b-nav-item>
-        <b-nav-item to="/auth/login" v-if="!CU.isLoggedIn()" class="mr-1">
-          <button class="btn-sm btn-dark">
-            <b-icon-check-square-fill></b-icon-check-square-fill>
-            {{ $t("TXT_LOGIN") }}
-          </button>
-        </b-nav-item>
-        <b-nav-item to="/auth/register" v-if="!CU.isLoggedIn()" class="mr-1">
-          <button class="btn-sm btn-dark">
-            <b-icon-person-check></b-icon-person-check>
-            {{ $t("TXT_REGISTER") }}
-          </button>
-        </b-nav-item>
       </b-navbar-nav>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <form autocomplete="off">
-            <b-form-select
-                size="sm"
-                v-model="languageSelected"
-                :options="languageList"
-            >
-            </b-form-select>
-          </form>
-
-          <b-nav-item-dropdown :text="$t('TXT_TOOLS')" left>
-            <b-dropdown-item :href="`${ConfigApi.url_base}/tools/SerializedDataEditor`">PHP-Serialized Data Editor online</b-dropdown-item>
-            <b-dropdown-item :href="`${ConfigApi.url_base}/tools/JsonSearchReplaceBeautify`">JSON Search-Replace-Beautify online</b-dropdown-item>
-            <b-dropdown-item to="/UrlParser">URL Parser + URL un-Parser (2 in 1)</b-dropdown-item>
-            <b-dropdown-item to="/CliParser">CLI Parser</b-dropdown-item>
-            <b-dropdown-item to="/Informer">Chronometer</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown :text="$t('TXT_TOOLS_ADMIN')" left v-if="CU.isAdmin()">
-            <b-dropdown-item to="/RestCall">HTTP calls with browser</b-dropdown-item>
-            <b-dropdown-item :href="`${ConfigApi.url_base}/SendRestApiQueries/BaseCurlCalls`">HTTP calls with server</b-dropdown-item>
-            <b-dropdown-item :href="`${ConfigApi.url_base}/AdminDbManager/DbTablesColumnsInfo`">DB Manger</b-dropdown-item>
-            <b-dropdown-item :href="`${ConfigApi.url_base}/admin/users`">User Management</b-dropdown-item>
-            <b-dropdown-item to="/about">UI tests</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-item-dropdown right>
             <!-- Using 'button-content' slot -->
             <template slot="button-content">
-              <!--<em>{{this.CU.name()}}</em>-->
               <img :src="CU.attributes.emblem" :alt="CU.attributes.firstname" height="40px" v-if="CU.attributes.emblem">
               <img v-if="!CU.isLoggedIn() || !CU.attributes.emblem" src="@/assets/anarki.png" height="40px">
               <span class="mr-1 ml-1">{{ CU.attributes.firstname || CU.attributes.mail || '¯\_(ツ)_/¯' }}</span>
             </template>
             <b-dropdown-item to="/auth/login" v-if="!CU.isLoggedIn()">{{ $t("TXT_LOGIN") }}</b-dropdown-item>
-            <b-dropdown-item to="/auth/register" v-if="!CU.isLoggedIn()">{{ $t("TXT_REGISTER") }}</b-dropdown-item>
-            <b-dropdown-item to="/auth/reset_password_request" v-if="!CU.isLoggedIn()">{{ $t("TXT_RESET_PASS_REQUEST") }}</b-dropdown-item>
-            <b-dropdown-item to="/auth/reset_password_with_code" v-if="!CU.isLoggedIn()">{{ $t("TXT_RESET_PASS_CODE") }}</b-dropdown-item>
             <!---->
             <b-dropdown-item to="/tale/upsert" v-if="CU.isLoggedIn()">{{ $t("TXT_ADD_TALE") }}</b-dropdown-item>
             <b-dropdown-item to="/auth/profile" v-if="CU.isLoggedIn()">{{ $t("TXT_PROFILE") }}</b-dropdown-item>
