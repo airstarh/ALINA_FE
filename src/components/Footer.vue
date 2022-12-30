e404567064aca0f0e2c826ca627ad0c5e5c7a034
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2022-12-30 17:13:06 +0300

    SSS

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="row no-gutters mt-5 alina-form p-5">
     <div class="col">
-      (c) localhost
+      Здесь люди просто тусят.
+
+      Коллектив индивидуалистов.
+
+      Славные ублюдки.
+
+      Идейная синь.
+
+      ОчУмелые ручки.
+
+      Лесная аристократия.
+
+      И прочие достойные персонажи.
+      <br>
+      <br>
+      Муза куража: <a href="https://www.facebook.com/sllimrut">Slimrut</a>
+      <br>
+      Ведущий проекта: <a href="https://vk.com/vsevolod.azovsky">Всеволод Азовский</a>
+      <br>
     </div>
   </div>
 </template>
