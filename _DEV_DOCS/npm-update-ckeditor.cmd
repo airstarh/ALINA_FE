f381784b93e1dfea28f3bf98a3d31999764e9d1b
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-03-16 18:06:48 +0600

    SSS CFG

---
+++ _DEV_DOCS/npm-update-ckeditor.cmd
@@ -0,0 +1,40 @@
+npm install
+@ckeditor/ckeditor5-vue2@latest
+@ckeditor/ckeditor5-dev-webpack-plugin@latest
+@ckeditor/ckeditor5-dev-utils@latest
+
+@ckeditor/ckeditor5-alignment@32.0.0
+@ckeditor/ckeditor5-basic-styles@32.0.0
+@ckeditor/ckeditor5-build-classic@32.0.0
+@ckeditor/ckeditor5-ckfinder@32.0.0
+@ckeditor/ckeditor5-cloud-services@32.0.0
+@ckeditor/ckeditor5-code-block@32.0.0
+@ckeditor/ckeditor5-easy-image@32.0.0
+@ckeditor/ckeditor5-editor-classic@32.0.0
+@ckeditor/ckeditor5-essentials@32.0.0
+@ckeditor/ckeditor5-font@32.0.0
+@ckeditor/ckeditor5-image@32.0.0
+@ckeditor/ckeditor5-indent@32.0.0
+@ckeditor/ckeditor5-link@32.0.0
+@ckeditor/ckeditor5-list@32.0.0
+@ckeditor/ckeditor5-paragraph@32.0.0
+@ckeditor/ckeditor5-paste-from-office@32.0.0
+@ckeditor/ckeditor5-remove-format@32.0.0
+@ckeditor/ckeditor5-table@32.0.0
+@ckeditor/ckeditor5-theme-lark@32.0.0
+@ckeditor/ckeditor5-typing@32.0.0
+@ckeditor/ckeditor5-upload@32.0.0
+@ckeditor/ckeditor5-utils@32.0.0
+@ckeditor/ckeditor5-media-embed@32.0.0
+
+########################################
+# NO PROBLEM
+npm install
+@ckeditor/ckeditor5-dev-utils@latest
+@ckeditor/ckeditor5-dev-webpack-plugin@latest
+
+########################################
+# OFFICIALLY
+# https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/frameworks/vuejs-v2.html
+postcss-loader@4
+raw-loader@4
