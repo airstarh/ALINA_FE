6739eb5b329bf437242c73ed7f378eb522bb73c2
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2023-03-18 13:42:58 +0600

    dev upldate npm nvm fuck fuck fuck

---
+++ _DEV_DOCS/batniki/NPM/npm-update-ckeditor-v.36.cmd
@@ -0,0 +1,83 @@
+:: https://ckeditor.com/docs/ckeditor5/latest/installation/frameworks/vuejs-v2.html
+npm install --save
+    @ckeditor/ckeditor5-vue2@latest
+    @ckeditor/ckeditor5-dev-translations@latest
+    @ckeditor/ckeditor5-dev-utils@latest
+    postcss-loader@4
+    raw-loader@4
+
+npm uninstall @ckeditor/ckeditor5-dev-webpack-plugin
+
+npm install
+@ckeditor/ckeditor5-alignment@latest
+@ckeditor/ckeditor5-basic-styles@latest
+@ckeditor/ckeditor5-build-classic@latest
+@ckeditor/ckeditor5-ckfinder@latest
+@ckeditor/ckeditor5-cloud-services@latest
+@ckeditor/ckeditor5-code-block@latest
+@ckeditor/ckeditor5-easy-image@latest
+@ckeditor/ckeditor5-editor-classic@latest
+@ckeditor/ckeditor5-essentials@latest
+@ckeditor/ckeditor5-font@latest
+@ckeditor/ckeditor5-image@latest
+@ckeditor/ckeditor5-indent@latest
+@ckeditor/ckeditor5-link@latest
+@ckeditor/ckeditor5-list@latest
+@ckeditor/ckeditor5-media-embed@latest
+@ckeditor/ckeditor5-paragraph@latest
+@ckeditor/ckeditor5-paste-from-office@latest
+@ckeditor/ckeditor5-remove-format@latest
+@ckeditor/ckeditor5-table@latest
+@ckeditor/ckeditor5-theme-lark@latest
+@ckeditor/ckeditor5-typing@latest
+@ckeditor/ckeditor5-upload@latest
+@ckeditor/ckeditor5-utils@latest
+
+:: ##################################################
+:: ##################################################
+:: ##################################################
+:: ##################################################
+
+
+ ERROR  Failed to compile with 1 error                                                                                                                                                                                             9:26:37 AM
+
+ error  in ./node_modules/@ckeditor/ckeditor5-image/theme/imageresize.css
+
+Error: PostCSS plugin postcss-import requires PostCSS 8.
+Migration guide for end-users:
+https://github.com/postcss/postcss/wiki/PostCSS-8-for-end-users
+
+
+ @ ./node_modules/@ckeditor/ckeditor5-image/src/imageresize.js 18:0-34
+ @ ./src/configs/ConfigCkEditor.js
+ @ ./node_modules/babel-loader/lib??ref--13-0!./node_modules/cache-loader/dist/cjs.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/MenuHorizontalMain.vue?vue&type=script&lang=js&
+ @ ./src/components/MenuHorizontalMain.vue?vue&type=script&lang=js&
+ @ ./src/components/MenuHorizontalMain.vue
+ @ ./node_modules/babel-loader/lib??ref--13-0!./node_modules/cache-loader/dist/cjs.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options!./src/App.vue?vue&type=script&lang=js&
+ @ ./src/App.vue?vue&type=script&lang=js&
+ @ ./src/App.vue
+ @ ./src/main.js
+ @ multi ./src/main.js
+
+ ERROR  Build failed with errors.
+C:\_A001\REPOS\OWN\vuesandbox\sandbox>
+npm -v
+7.20.1
+C:\_A001\REPOS\OWN\vuesandbox\sandbox>
+npm install -g n
+npm ERR! code EBADPLATFORM
+npm ERR! notsup Unsupported platform for n@9.0.1: wanted {"os":"!win32"} (current: {"os":"win32","arch":"x64"})
+npm ERR! notsup Valid OS:    !win32
+npm ERR! notsup Valid Arch:  undefined
+npm ERR! notsup Actual OS:   win32
+npm ERR! notsup Actual Arch: x64
+
+npm ERR! A complete log of this run can be found in:
+npm ERR!     C:\Users\vazovskiy\AppData\Local\npm-cache\_logs\2023-03-18T03_42_19_973Z-debug.log
+C:\_A001\REPOS\OWN\vuesandbox\sandbox>
+
+C:\_A001\REPOS\OWN\vuesandbox\sandbox>
+
+C:\_A001\REPOS\OWN\vuesandbox\sandbox>
+
+
