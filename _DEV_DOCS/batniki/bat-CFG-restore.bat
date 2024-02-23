039d88c3372fbf1ec38ec6bc97d5e28f89995955
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2024-02-23 05:42:36 +0300

    _DEV_DOCS

--- _DEV_DOCS/batniki/bat-CFG-restore.bat
+++ _DEV_DOCS/batniki/bat-CFG-restore.bat
@@ -9,14 +9,23 @@
 ::set PROJ=vov
 ::set PROJ=sss
 set PROJ=localhost
-set FROM=C:\_A001\REPOS\OWN\vuesandbox\sandbox\_DEV_DOCS\batniki\_CFG\%PROJ%
-set TOTO=C:\_A001\REPOS\OWN\vuesandbox\sandbox
+set FROM=C:\_A001\REPOS\OWN\ALINA_FE\_DEV_DOCS\batniki\_CFG\%PROJ%
+set TOTO=C:\_A001\REPOS\OWN\ALINA_FE
+
+::##################################################
 :: DIRECTORIES
-xcopy /i /e /y %FROM%\public %TOTO%\public
 xcopy /i /e /y %FROM%\src\assets %TOTO%\src\assets
+
+::##################################################
 :: FILES
 xcopy /y %FROM%\src\configs\ConfigApi.js %TOTO%\src\configs\
+
 xcopy /y %FROM%\src\components\MenuHorizontalMain.vue %TOTO%\src\components\
 xcopy /y %FROM%\src\components\Footer.vue %TOTO%\src\components\
+
+xcopy /y %FROM%\public\favicon.ico %TOTO%\public\
+xcopy /y %FROM%\public\favicon.svg %TOTO%\public\
+xcopy /y %FROM%\public\manifest.json %TOTO%\public\
+xcopy /y %FROM%\public\index.html %TOTO%\public\
 
 pause
--- _DEV_DOCS/batniki/bat-CFG-save.bat
+++ _DEV_DOCS/batniki/bat-CFG-save.bat
@@ -1,5 +1,5 @@
 ::
-:: SSS
+:: ALINA_FE
 :: Documentation:
 :: https://learn.microsoft.com/en-US/troubleshoot/windows-client/deployment/switches-with-xcopy-and-xcopy32-command?ranMID=43674&ranEAID=FE4O7wtxe6g&ranSiteID=FE4O7wtxe6g-S.IT_QZ3nGxJeSPxTJCMGQ&epi=FE4O7wtxe6g-S.IT_QZ3nGxJeSPxTJCMGQ&irgwc=1&OCID=AID2200057_aff_7795_1243925&tduid=(ir__o3dn266zwgkfblmvlgudtjkavu2xcgprnll9o3j900)(7795)(1243925)(FE4O7wtxe6g-S.IT_QZ3nGxJeSPxTJCMGQ)()&irclickid=_o3dn266zwgkfblmvlgudtjkavu2xcgprnll9o3j900
 ::
@@ -8,14 +8,22 @@
 ::set PROJ=vov
 ::set PROJ=sss
 set PROJ=localhost
-set FROM=C:\_A001\REPOS\OWN\vuesandbox\sandbox
-set TOTO=C:\_A001\REPOS\OWN\vuesandbox\sandbox\_DEV_DOCS\batniki\_CFG\%PROJ%
+set FROM=C:\_A001\REPOS\OWN\ALINA_FE
+set TOTO=C:\_A001\REPOS\OWN\ALINA_FE\_DEV_DOCS\batniki\_CFG\%PROJ%
+
+::##################################################
 :: DIRECTORIES
-xcopy /i /e /y %FROM%\public %TOTO%\public
 xcopy /i /e /y %FROM%\src\assets %TOTO%\src\assets
+
+::##################################################
 :: FILES
 xcopy /y %FROM%\src\configs\ConfigApi.js %TOTO%\src\configs\
+
 xcopy /y %FROM%\src\components\MenuHorizontalMain.vue %TOTO%\src\components\
 xcopy /y %FROM%\src\components\Footer.vue %TOTO%\src\components\
 
+xcopy /y %FROM%\public\favicon.ico %TOTO%\public\
+xcopy /y %FROM%\public\favicon.svg %TOTO%\public\
+xcopy /y %FROM%\public\manifest.json %TOTO%\public\
+xcopy /y %FROM%\public\index.html %TOTO%\public\
 pause
