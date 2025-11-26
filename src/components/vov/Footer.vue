5dc0c5a6d23234cd088549c36236dc555266c9b7
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2025-11-27 01:46:57 +0300

    env

---
+++ src/components/vov/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mt-5 alina-form p-5">
+    Мемориальный комплекс "Осетровский плацдарм"
+    <a href="https://oixm.ru/" target="_blank">
+      Острогожского историко-художественного музея им. И.Н. Крамского
+    </a>
+    <br>
+    Воронежская область, Верхнемамонский район, 720 км федеральной автомобильной дороги М4 «Дон».
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {}
+  },
+  methods: {}
+};
+</script>
+
+<style scoped lang="scss"></style>
