3ecb095970f0f7b034bec3b1f51864ddc4e6c6d4
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2025-12-25 19:44:26 +0300

    stage dev

---
+++ src/components/stage/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row no-gutters mt-5 alina-form p-5">
+    <div class="col">
+      (c) STAGE
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {}
+  },
+  methods: {}
+};
+</script>
+
+<style scoped lang="scss"></style>
