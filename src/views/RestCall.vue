27d1d0bae7e2f442c3eea3d7a1a60f1b69c55f39
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2026-01-05 06:20:13 +0300

    dev RestCall.vue attention not tested

--- src/views/RestCall.vue
+++ src/views/RestCall.vue
@@ -12,7 +12,7 @@
           </div>
           <div class="form-group mt-3">
             <BtstrpBadge title="URI" badge="reqUri"></BtstrpBadge>
-            <input v-model="reqUri" type="text" class="form-control"/>
+            <input v-model="reqUri" type="text" class="form-control" />
           </div>
           <!--##################################################-->
           <!--##################################################-->
@@ -21,37 +21,33 @@
             <div class="col-sm">
               <div>Method</div>
               <b-form-select
-                  v-model="reqMethod"
-                  :options="reqMethods"
-                  label="Method"
-              ></b-form-select>
+                v-model="reqMethod"
+                :options="reqMethods"
+                label="Method"></b-form-select>
             </div>
 
             <div class="col-sm">
               <div>Mode</div>
               <b-form-select
-                  v-model="mode"
-                  :options="modes"
-                  label="Mode"
-              ></b-form-select>
+                v-model="mode"
+                :options="modes"
+                label="Mode"></b-form-select>
             </div>
 
             <div class="col-sm">
               <div>Credential</div>
               <b-form-select
-                  v-model="credentials"
-                  :options="credentialsList"
-                  label="Credential"
-              ></b-form-select>
+                v-model="credentials"
+                :options="credentialsList"
+                label="Credential"></b-form-select>
             </div>
 
             <div class="col-sm">
               <div>Enctype</div>
               <b-form-select
-                  v-model="enctype"
-                  :options="enctypes"
-                  label="Enctype"
-              ></b-form-select>
+                v-model="enctype"
+                :options="enctypes"
+                label="Enctype"></b-form-select>
             </div>
           </div>
           <!--##################################################-->
@@ -59,13 +55,14 @@
           <!--##################################################-->
           <div class="row">
             <div class="col-sm">
-              <ui-fileupload
-                  accept="image/*"
-                  :multiple="true "
-                  name="userfile"
-                  @change="onChangeFileField"
-              >Select an image
-              </ui-fileupload>
+              <aInput
+                type="file"
+                :modeEdit="true"
+                label="Select an image"
+                accept="image/*"
+                :multiple="true"
+                name="userfile"
+                @change="onChangeFileField" />
             </div>
           </div>
           <!--##################################################-->
@@ -83,7 +80,7 @@
                 <BtstrpBadge title="POST" badge="reqPost"></BtstrpBadge>
                 <textarea id="reqPost" v-model="reqPostTxt" class="form-control" rows="11"></textarea>
                 <div class="custom-control custom-checkbox">
-                  <input id="reqFlagPostRaw" v-model="reqFlagPostRaw" type="checkbox" class="custom-control-input"/>
+                  <input id="reqFlagPostRaw" v-model="reqFlagPostRaw" type="checkbox" class="custom-control-input" />
                   <label class="btn btn-primary custom-control-label" for="reqFlagPostRaw">
                     reqFlagPostRaw
                   </label>
@@ -110,7 +107,7 @@
           <h2>Response</h2>
           <div class="mt-3">
             <BtstrpBadge title="URI" badge="urlRes"></BtstrpBadge>
-            <input v-model="urlRes" type="text" class="form-control"/>
+            <input v-model="urlRes" type="text" class="form-control" />
           </div>
 
           <div class="mt-3">
@@ -137,13 +134,15 @@
 
 <script>
 import StandardButtons from "../components/elements/form/StandardButtons";
-import BtstrpBadge     from "../components/elements/BtstrpBadge";
-import Ajax            from "../services/Ajax";
-import AjaxAlina       from "../services/AjaxAlina";
+import BtstrpBadge from "../components/elements/BtstrpBadge";
+import Ajax from "../services/Ajax";
+import AjaxAlina from "../services/AjaxAlina";
+import aInput from "@/components/elements/form/aInput";
 
 export default {
-  name:       "RestCall",
+  name: "RestCall",
   components: {
+    aInput,
     StandardButtons,
     BtstrpBadge
   },
@@ -154,78 +153,78 @@ export default {
       reqUri: "https://local.host:7002/eg/php/php-redirect-1.php?lala=qqq",
       /////////////////////////////////
       modes: ["cors", "no-cors", "*same-origin"],
-      mode:  "cors",
+      mode: "cors",
       /////////////////////////////////
       caches: ["no-cache", "*default", "no-cache", "reload", "force-cache", "only-if-cached"],
-      cache:  "no-cache",
+      cache: "no-cache",
       /////////////////////////////////
       credentialsList: ["omit", "same-origin", "include", "*omit"],
-      credentials:     "include",
+      credentials: "include",
       /////////////////////////////////
       redirects: ["follow", "manual", "*follow", "error"],
-      redirect:  "follow",
+      redirect: "follow",
       /////////////////////////////////
       referrers: ["no-referrer", "client"],
-      referrer:  "",
+      referrer: "",
       /////////////////////////////////
-      reqGet:    {
+      reqGet: {
         lala: [4, 5, 6],
-        foo:  'привет'
+        foo: 'привет'
       },
       reqGetTxt: "",
       /////////////////////////////////
-      reqPost:         {
-        "id":               "1",
-        "mail":             "vsevolod.azovsky@gmail.com",
-        "firstname":        "Sewa",
-        "lastname":         "Azovsky",
-        "emblem":           "111",
-        "birth":            "477100800",
-        "language":         "en",
-        "timezone":         "3",
-        "is_verified":      "1",
-        "banned_till":      "1277928000",
-        "created_at":       "1569877200",
-        "is_deleted":       "0",
-        "last_time":        "1580058021",
+      reqPost: {
+        "id": "1",
+        "mail": "vsevolod.azovsky@gmail.com",
+        "firstname": "Sewa",
+        "lastname": "Azovsky",
+        "emblem": "111",
+        "birth": "477100800",
+        "language": "en",
+        "timezone": "3",
+        "is_verified": "1",
+        "banned_till": "1277928000",
+        "created_at": "1569877200",
+        "is_deleted": "0",
+        "last_time": "1580058021",
         "last_browser_enc": "09f454857cbf37627aacee60db0bd6fc",
-        "last_ip":          "127.0.0.1",
-        "fingerprint":      "",
-        "about_myself":     "111333",
-        "reset_code":       "1579611464",
-        "reset_required":   "1",
-        "timezone_name":    "The Third Тайм Зона",
-        "form_id":          "actionProfile"
+        "last_ip": "127.0.0.1",
+        "fingerprint": "",
+        "about_myself": "111333",
+        "reset_code": "1579611464",
+        "reset_required": "1",
+        "timezone_name": "The Third Тайм Зона",
+        "form_id": "actionProfile"
       },
-      reqPostTxt:      "",
-      reqFlagPostRaw:  false,
+      reqPostTxt: "",
+      reqFlagPostRaw: false,
       reqFlagPostFile: false,
-      reqFileList:     [],
-      enctypes:        ["application/json", "multipart/form-data", "text-plain", "application/x-www-form-urlencoded"],
-      enctype:         "multipart/form-data",
+      reqFileList: [],
+      enctypes: ["application/json", "multipart/form-data", "text-plain", "application/x-www-form-urlencoded"],
+      enctype: "multipart/form-data",
       /////////////////////////////////
-      reqHeaders:    {
+      reqHeaders: {
         "fgp": "sewa"
       },
       reqHeadersTxt: "",
       /////////////////////////////////
-      reqCookie:    {
+      reqCookie: {
         "salam": "пополам",
       },
       reqCookieTxt: '',
       /////////////////////////////////
       reqMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      reqMethod:  "GET",
+      reqMethod: "GET",
       //endregion Request
       /////////////////////////////////
       /////////////////////////////////
       /////////////////////////////////
       //region Response
-      urlRes:      "",
-      respBody:    "",
+      urlRes: "",
+      respBody: "",
       respBodyTxt: "",
       /////////////////////////////////
-      respHeaders:   {},
+      respHeaders: {},
       respHeaderTxt: '',
       //endregion Response
       /////////////////////////////////
@@ -239,25 +238,25 @@ export default {
     runAJax() {
       this.strsToObjs();
       const oAjax = Ajax.newInst({
-        url:            this.reqUri,
-        mode:           this.mode,
-        cache:          this.cache,
-        credentials:    this.credentials,
-        redirect:       this.redirect,
-        referrer:       this.referrer,
-        getParams:      this.reqGet,
-        headers:        this.reqHeaders,
-        enctype:        this.enctype,
+        url: this.reqUri,
+        mode: this.mode,
+        cache: this.cache,
+        credentials: this.credentials,
+        redirect: this.redirect,
+        referrer: this.referrer,
+        getParams: this.reqGet,
+        headers: this.reqHeaders,
+        enctype: this.enctype,
         reqFlagPostRaw: this.reqFlagPostRaw,
-        postParams:     this.reqPost,
-        method:         this.reqMethod,
-        onDone:         this.onDone
+        postParams: this.reqPost,
+        method: this.reqMethod,
+        onDone: this.onDone
       });
       oAjax.go();
     },
 
     onDone(oAjax) {
-      this.respHeaders   = oAjax.respHeaders;
+      this.respHeaders = oAjax.respHeaders;
       this.respHeaderTxt = JSON.stringify(this.respHeaders, null, 2);
       switch (oAjax.respType) {
         case "json":
@@ -270,9 +269,9 @@ export default {
           break;
       }
 
-      this.reqUri  = oAjax.urlClean;
-      this.urlRes  = oAjax.urlRes;
-      this.reqGet  = oAjax.options.getParams;
+      this.reqUri = oAjax.urlClean;
+      this.urlRes = oAjax.urlRes;
+      this.reqGet = oAjax.options.getParams;
       this.reqPost = oAjax.options.postParams;
       //#####
       delete this.reqHeaders.token;
@@ -290,8 +289,8 @@ export default {
 
     objsToTxt() {
       this.reqHeadersTxt = JSON.stringify(this.reqHeaders, null, 6);
-      this.reqCookieTxt  = JSON.stringify(this.reqCookie, null, 6);
-      this.reqGetTxt     = JSON.stringify(this.reqGet, null, 6);
+      this.reqCookieTxt = JSON.stringify(this.reqCookie, null, 6);
+      this.reqGetTxt = JSON.stringify(this.reqGet, null, 6);
       if (!this.reqFlagPostRaw) {
         this.reqPostTxt = JSON.stringify(this.reqPost, null, 6);
       }
@@ -301,14 +300,14 @@ export default {
       //#####
       this.reqCookie = JSON.parse(this.reqCookieTxt);
       Object.entries(this.reqCookie).forEach((ind_val) => {
-        const ind       = ind_val[0];
-        const val       = ind_val[1];
+        const ind = ind_val[0];
+        const val = ind_val[1];
         document.cookie = `${ind}=${val}`;
       });
       //#####
       this.reqHeaders = JSON.parse(this.reqHeadersTxt);
       //#####
-      this.reqGet     = JSON.parse(this.reqGetTxt);
+      this.reqGet = JSON.parse(this.reqGetTxt);
       //#####
       if (this.reqFlagPostRaw) {
         this.reqPost = this.reqPostTxt;
@@ -321,7 +320,7 @@ export default {
       //#####
     },
 
-    onChangeFileField(fileList, event) {
+    onChangeFileField(fileList) {
       if (fileList.length > 0) {
         this.reqFlagPostFile = true;
       } else {
@@ -335,7 +334,7 @@ export default {
       }
     },
     cleanUpPreDefinedSets() {
-      this.reqGet  = {};
+      this.reqGet = {};
       this.reqPost = {};
       this.reqHeaders = {};
       this.reqCookie = {};
