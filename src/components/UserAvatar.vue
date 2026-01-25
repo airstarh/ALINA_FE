5d25cd1a374516a4262cb1d562a4ec4c5c6da9be
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-01-25 19:21:59 +0300

    dev

--- src/components/UserAvatar.vue
+++ src/components/UserAvatar.vue
@@ -1,97 +1,123 @@
 <template>
   <div class="UserAvatar">
-    <div class="text-right" v-if="!UtilsData.empty(someDate)">
-      <span style="font-size: min(4vmin, 0.7em);">{{ UtilsDate.fromUnixToDateTime(someDate) }}</span>
-    </div>
     <div
-        class="row no-gutters"
+      class="text-right"
+      v-if="!UtilsData.empty(someDate)"
     >
+      <span style="font-size: min(4vmin, 0.7em)">{{
+        UtilsDate.fromUnixToDateTime(someDate)
+      }}</span>
+    </div>
+    <div class="row no-gutters">
       <div class="col">
         <span
-            class="text-left text-nowrap p-2 alina-user-avatar-wrapper"
-            :style="{
-              'border-radius': isComment ? '0 25px 25px 0' :'25px'
-            }"
+          class="text-left text-nowrap p-2 alina-user-avatar-wrapper"
+          :style="{
+            'border-radius': isComment ? '0 25px 25px 0' : '25px',
+          }"
         >
-          <router-link :to="'/auth/profile/'+userId" class="text-light alina-user-avatar">
-            {{ UtilsStr.firstName(this.userFirstName, this.userLastName, this.userId) }}
+          <router-link
+            :to="'/auth/profile/' + userId"
+            class="text-light alina-user-avatar"
+          >
+            {{
+              UtilsStr.firstName(
+                this.userFirstName,
+                this.userLastName,
+                this.userId
+              )
+            }}
           </router-link>
           <span>&nbsp;</span>
-          <router-link :to="'/auth/profile/'+userId" class="">
-                <img :src="emblemUrl" class="rounded-circle" :style="{'width': emblemWidth}">
+          <router-link
+            :to="'/auth/profile/' + userId"
+            class=""
+          >
+            <img
+              :src="emblemUrl"
+              class="rounded-circle"
+              :style="{ width: emblemWidth }"
+            />
           </router-link>
           <span>&nbsp;</span>
-          <router-link :to="'/auth/profile/'+userId" class="text-light alina-user-avatar">
-            {{ UtilsStr.lastName(this.userFirstName, this.userLastName, this.userId) }}
+          <router-link
+            :to="'/auth/profile/' + userId"
+            class="text-light alina-user-avatar"
+          >
+            {{
+              UtilsStr.lastName(
+                this.userFirstName,
+                this.userLastName,
+                this.userId
+              )
+            }}
           </router-link>
         </span>
-
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import UtilsStr  from "@/Utils/UtilsStr";
+import UtilsStr from "@/Utils/UtilsStr";
 import UtilsDate from "../Utils/UtilsDate";
 import UtilsData from "@/Utils/UtilsData";
 
 export default {
-  name:     "UserAvatar",
+  name: "UserAvatar",
   computed: {
     UtilsData() {
-      return UtilsData
+      return UtilsData;
     },
     UtilsDate() {
-      return UtilsDate
-    }
+      return UtilsDate;
+    },
   },
-  props:    {
-    userId:         {
-      type:    String | Number,
-      default: ''
+  props: {
+    userId: {
+      type: String | Number,
+      default: "",
     },
-    userFirstName:  {
-      type:    String,
-      default: ''
+    userFirstName: {
+      type: String,
+      default: "",
     },
-    userLastName:   {
-      type:    String,
-      default: ''
+    userLastName: {
+      type: String,
+      default: "",
     },
-    someDate:       {
-      type:    String | Number,
-      default: ''
+    someDate: {
+      type: String | Number,
+      default: "",
     },
-    emblemUrl:      {
-      type:    String,
-      default: '@/assets/anarki.png'
+    emblemUrl: {
+      type: String,
+      default: "@/assets/anarki.png",
     },
-    emblemWidth:    {
-      type:    String,
-      default: '50px'
+    emblemWidth: {
+      type: String,
+      default: "50px",
     },
     additionalInfo: {
-      type:    String,
-      default: ''
+      type: String,
+      default: "",
     },
-    isComment:      {
-      type:    Boolean,
-      default: false
+    isComment: {
+      type: Boolean,
+      default: false,
     },
   },
   data() {
     return {
-      UtilsStr
-    }
+      UtilsStr,
+    };
   },
-  methods: {}
+  methods: {},
 };
 </script>
 
 <style scoped lang="scss">
 @media (max-width: 500px) {
-
 }
 
 .alina-user-avatar-wrapper {
@@ -107,5 +133,4 @@ a.alina-user-avatar {
   white-space: nowrap;
   vertical-align: middle;
 }
-
 </style>
