97a52954e8b94fbf4986d0ea2cdda0bbffd54121
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-01-30 17:02:47 +0300

    dev s

--- src/components/elements/form/Share.vue
+++ src/components/elements/form/Share.vue
@@ -1,121 +1,169 @@
 <template>
-<span>
-<ShareNetwork
-    network="VK"
-    :url="hrefToBackend"
-    :title="`${tale.header}`"
-    :description="UtilsStr.truncate(tale.body_txt, 100)"
->
-    <button class="btn p-1" title="vkontakte">
-        <svg height="1.8rem" width="1.8rem" class="corporate-svg-fill">
-            <use :xlink:href="`${iconVk.url}`"></use>
+  <span>
+    <ShareNetwork
+      network="VK"
+      :url="hrefToBackend"
+      :title="`${tale.header}`"
+      :description="UtilsStr.truncate(tale.body_txt, 100)"
+    >
+      <button
+        class="btn p-1"
+        title="vkontakte"
+      >
+        <svg
+          height="1.8rem"
+          width="1.8rem"
+          class="corporate-svg-fill"
+        >
+          <use :xlink:href="`${iconVk.url}`"></use>
         </svg>
-    </button>
-</ShareNetwork>
-<ShareNetwork
-    network="facebook"
-    :url="hrefToBackend"
-    :title="`${tale.header}`"
-    :description="UtilsStr.truncate(tale.body_txt, 100)"
->
-    <button class="btn p-1" title="facebook">
-        <svg height="1.8rem" width="1.8rem" class="corporate-svg-fill">
-            <use :xlink:href="`${iconFb.url}`"></use>
+      </button>
+    </ShareNetwork>
+    <ShareNetwork
+      network="facebook"
+      :url="hrefToBackend"
+      :title="`${tale.header}`"
+      :description="UtilsStr.truncate(tale.body_txt, 100)"
+    >
+      <button
+        class="btn p-1"
+        title="facebook"
+      >
+        <svg
+          height="1.8rem"
+          width="1.8rem"
+          class="corporate-svg-fill"
+        >
+          <use :xlink:href="`${iconFb.url}`"></use>
         </svg>
-    </button>
-</ShareNetwork>
-<ShareNetwork
-    network="Telegram"
-    :url="hrefToBackend"
-    :title="`${tale.header}`"
-    :description="UtilsStr.truncate(tale.body_txt, 100)"
->
-    <button class="btn p-1" title="Telegram">
-        <svg height="1.8rem" width="1.8rem" class="corporate-svg-fill">
-            <use :xlink:href="`${iconTg.url}`"></use>
+      </button>
+    </ShareNetwork>
+    <ShareNetwork
+      network="Telegram"
+      :url="hrefToBackend"
+      :title="`${tale.header}`"
+      :description="UtilsStr.truncate(tale.body_txt, 100)"
+    >
+      <button
+        class="btn p-1"
+        title="Telegram"
+      >
+        <svg
+          height="1.8rem"
+          width="1.8rem"
+          class="corporate-svg-fill"
+        >
+          <use :xlink:href="`${iconTg.url}`"></use>
         </svg>
-
-    </button>
-</ShareNetwork>
-<ShareNetwork
-    network="WhatsApp"
-    :url="hrefToBackend"
-    :title="`${tale.header}`"
-    :description="UtilsStr.truncate(tale.body_txt, 100)"
->
-    <button class="btn p-1" title="WhatsApp">
-        <svg height="1.8rem" width="1.8rem" class="corporate-svg-fill">
-            <use :xlink:href="`${iconWp.url}`"></use>
+      </button>
+    </ShareNetwork>
+    <ShareNetwork
+      network="WhatsApp"
+      :url="hrefToBackend"
+      :title="`${tale.header}`"
+      :description="UtilsStr.truncate(tale.body_txt, 100)"
+    >
+      <button
+        class="btn p-1"
+        title="WhatsApp"
+      >
+        <svg
+          height="1.8rem"
+          width="1.8rem"
+          class="corporate-svg-fill"
+        >
+          <use :xlink:href="`${iconWp.url}`"></use>
         </svg>
-    </button>
-</ShareNetwork>
-<ShareNetwork
-    network="Skype"
-    :url="hrefToBackend"
-    :title="`${tale.header}`"
-    :description="UtilsStr.truncate(tale.body_txt, 100)"
->
-<button class="btn p-1" title="Skype">
-    <svg height="1.8rem" width="1.8rem" class="corporate-svg-fill">
-        <use :xlink:href="`${iconSk.url}`"></use>
-    </svg>
-</button>
-</ShareNetwork>
-<ShareNetwork
-    network="Viber"
-    :url="hrefToBackend"
-    :title="`${tale.header}`"
-    :description="UtilsStr.truncate(tale.body_txt, 100)"
->
-<button class="btn p-1" title="Viber">
-<svg height="1.8rem" width="1.8rem" class="corporate-svg-fill">
-    <use :xlink:href="`${iconVb.url}`"></use>
-</svg>
-</button>
-</ShareNetwork>
-<ShareNetwork
-    network="LinkedIn"
-    :url="hrefToBackend"
-    :title="`${tale.header}`"
-    :description="UtilsStr.truncate(tale.body_txt, 100)"
->
-<button class="btn p-1" title="LinkedIn">
-<svg height="1.8rem" width="1.8rem" class="corporate-svg-fill">
-<use :xlink:href="`${iconIn.url}`"></use>
-</svg>
-</button>
-</ShareNetwork>
-</span>
+      </button>
+    </ShareNetwork>
+    <ShareNetwork
+      network="Skype"
+      :url="hrefToBackend"
+      :title="`${tale.header}`"
+      :description="UtilsStr.truncate(tale.body_txt, 100)"
+    >
+      <button
+        class="btn p-1"
+        title="Skype"
+      >
+        <svg
+          height="1.8rem"
+          width="1.8rem"
+          class="corporate-svg-fill"
+        >
+          <use :xlink:href="`${iconSk.url}`"></use>
+        </svg>
+      </button>
+    </ShareNetwork>
+    <ShareNetwork
+      network="Viber"
+      :url="hrefToBackend"
+      :title="`${tale.header}`"
+      :description="UtilsStr.truncate(tale.body_txt, 100)"
+    >
+      <button
+        class="btn p-1"
+        title="Viber"
+      >
+        <svg
+          height="1.8rem"
+          width="1.8rem"
+          class="corporate-svg-fill"
+        >
+          <use :xlink:href="`${iconVb.url}`"></use>
+        </svg>
+      </button>
+    </ShareNetwork>
+    <ShareNetwork
+      network="LinkedIn"
+      :url="hrefToBackend"
+      :title="`${tale.header}`"
+      :description="UtilsStr.truncate(tale.body_txt, 100)"
+    >
+      <button
+        class="btn p-1"
+        title="LinkedIn"
+      >
+        <svg
+          height="1.8rem"
+          width="1.8rem"
+          class="corporate-svg-fill"
+        >
+          <use :xlink:href="`${iconIn.url}`"></use>
+        </svg>
+      </button>
+    </ShareNetwork>
+  </span>
 </template>
 
 <script>
 import ConfigApi from "@/configs/ConfigApi";
-import UtilsStr  from "@/Utils/UtilsStr";
-import iconVk    from "@/assets/svg/socialnets/vk.svg";
-import iconFb    from "@/assets/svg/socialnets/fb.svg";
-import iconWp    from "@/assets/svg/socialnets/whatsapp.svg";
-import iconTg    from "@/assets/svg/socialnets/telgram.svg";
-import iconSk    from "@/assets/svg/socialnets/skype.svg";
-import iconVb    from "@/assets/svg/socialnets/viber.svg";
-import iconIn    from "@/assets/svg/socialnets/linkedin.svg";
-import UtilsSys  from "@/Utils/UtilsSys";
+import UtilsStr from "@/Utils/UtilsStr";
+import iconVk from "@/assets/svg/socialnets/vk.svg";
+import iconFb from "@/assets/svg/socialnets/fb.svg";
+import iconWp from "@/assets/svg/socialnets/whatsapp.svg";
+import iconTg from "@/assets/svg/socialnets/telgram.svg";
+import iconSk from "@/assets/svg/socialnets/skype.svg";
+import iconVb from "@/assets/svg/socialnets/viber.svg";
+import iconIn from "@/assets/svg/socialnets/linkedin.svg";
+import UtilsSys from "@/Utils/UtilsSys";
 export default {
-  name:     "Share",
+  name: "Share",
   computed: {
     hrefToBackend() {
-      return UtilsSys.hrefToBackend(this.tale, 'tale/upsert')
-    }
+      return UtilsSys.hrefToBackend(this.tale, "tale/upsert");
+    },
   },
-  props:    {
+  props: {
     tale: {
-      type:    Object,
+      type: Object,
       default: () => {
         return {
-          id:       null,
+          id: null,
           body_txt: null,
         };
-      }
-    }
+      },
+    },
   },
   data() {
     return {
@@ -127,11 +175,10 @@ export default {
       iconWp,
       iconTg,
       UtilsStr,
-      ConfigApi
-    }
-  }
+      ConfigApi,
+    };
+  },
 };
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
