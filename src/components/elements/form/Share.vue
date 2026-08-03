3a9feea337b373f02f8443df5c2110da21e73dca
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-08-03 16:10:11 +0300

    dev

--- src/components/elements/form/Share.vue
+++ src/components/elements/form/Share.vue
@@ -3,25 +3,22 @@
     <button
       class="btn btn-sm btn-secondary rounded-circle"
       @click="copyToBuffer"
-      :title="$t('Copy link')"
-    >
-    <b-icon icon="share"></b-icon>
+      :title="$t('Copy link')">
+      <b-icon icon="share" v-if="!stateCopying"></b-icon>
+      <b-icon icon="check" v-if="stateCopying"></b-icon>
     </button>
     <ShareNetwork
       network="VK"
       :url="hrefToBackend"
       :title="`${tale.header}`"
-      :description="UtilsStr.truncate(tale.body_txt, 100)"
-    >
+      :description="UtilsStr.truncate(tale.body_txt, 100)">
       <button
         class="btn p-1"
-        title="vkontakte"
-      >
+        title="vkontakte">
         <svg
           height="1.8rem"
           width="1.8rem"
-          class="corporate-svg-fill"
-        >
+          class="corporate-svg-fill">
           <use :xlink:href="`${iconVk.url}`"></use>
         </svg>
       </button>
@@ -33,11 +30,6 @@
 import ConfigApi from "@/configs/ConfigApi";
 import UtilsStr from "@/Utils/UtilsStr";
 import iconVk from "@/assets/svg/socialnets/vk.svg";
-import iconFb from "@/assets/svg/socialnets/fb.svg";
-import iconWp from "@/assets/svg/socialnets/whatsapp.svg";
-import iconTg from "@/assets/svg/socialnets/telgram.svg";
-import iconVb from "@/assets/svg/socialnets/viber.svg";
-import iconIn from "@/assets/svg/socialnets/linkedin.svg";
 import UtilsSys from "@/Utils/UtilsSys";
 export default {
   name: "Share",
@@ -59,19 +51,17 @@ export default {
   },
   data() {
     return {
-      iconIn,
-      iconVb,
-      iconFb,
       iconVk,
-      iconWp,
-      iconTg,
       UtilsStr,
       ConfigApi,
+      stateCopying: false,
     };
   },
   methods: {
     copyToBuffer() {
+      this.stateCopying = true;
       navigator.clipboard.writeText(this.hrefToBackend);
+      setTimeout(() => { this.stateCopying = false }, 3000);
     },
   },
 };
